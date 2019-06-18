5e288d2ecc5fa38ebb8b6e42c8b490dc9ed1a413
liusiqi <liusiqi@liusiqideMacBook-Pro.local>
2019-06-18 22:35:32 +0800

    tpl

--- src/helper/router.js
+++ src/helper/router.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const Handlebars = require('handlebars');
+const path = require('path');
+const tplPath = path.join(__dirname,'../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
+const config = require('../config/defaultConfig');
+
 module.exports = async function(req, res, filePath ) {
   try {
     const stats = await stat(filePath);
@@ -13,7 +20,12 @@ module.exports = async function(req, res, filePath ) {
         const files = await readdir(filePath);
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/plain');
-        res.end(files.join(','));
+        const data = {
+          title: path.basename(filePath),
+          dir: path.relative(config.root, filePath),
+          files,
+        }
+        res.end(template(data));
     }
   } catch (error) {
     res.statusCode = 404;

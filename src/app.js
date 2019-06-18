dbf762b797b1cd38a035735c1a02528769d1d95c
liusisi <liusisi@163.com>
2019-06-18 11:29:37 +0800

    helper

--- src/app.js
+++ src/app.js
@@ -1,32 +1,15 @@
 const http = require('http');
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const path = require('path');
 const conf = require('./config/defaultConfig');
-const fs = require('fs');
+const router = require('./helper/router')
 
 const server = http.createServer((req,res)=>{
   const filePath = path.join(conf.root, req.url);
-  fs.stat(filePath, (err, stats) => {
-    if(err){
-      res.statusCode = 404;
-      res.setHeader('Content-type', 'text/plain');
-      res.end(`${filePath} is not a direction or file`);
-    }
-    if(stats.isFile()){
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'text/plain');
-      fs.createReadStream(filePath).pipe(res);
-    }else if(stats.isDirectory()){
-      fs.readdir(filePath,(err, files) => {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/plain');
-        res.end(files.join(','));
-      })
-    }
-  })
+  router(req, res, filePath);
+  
 });
 
 server.listen(conf.port, conf.hostname, ()=>{
-  const addr = `http://${conf.hostname}:${conf.port}`;
-  console.info(`Server start at ${chalk.green(addr)}`);
+  // const addr = `http://${conf.hostname}:${conf.port}`;
 });
